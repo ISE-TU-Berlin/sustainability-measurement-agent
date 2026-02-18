35602a5a6d0c2ee084a7f7b801262182f6067998
Karl Wolf <wolfkarl@gmail.com>
2026-02-18 12:35:29 +0100

    allow namespaces in kubectl and use them for teardown

--- modules/telelocust/main.py
+++ modules/telelocust/main.py
@@ -106,7 +106,7 @@ class TelelocustSmaModule(SMAObserver, Triggerable):
         if self.__deploy_enabled():
             log.info(subprocess.run("pwd", shell=False, capture_output=True, text=True).stdout)
             log.info("Deploying TeleLocust infrastructure...")
-            self.__kubectl(f"apply -n {self.config.namespace}", DEPLOYMENT_YAML_PATH)
+            self.__kubectl(f"apply", DEPLOYMENT_YAML_PATH, namespace=self.config.namespace)
             time.sleep(1)  # Wait for deployment to stabilize
             log.info("TeleLocust infrastructure deployed.")
 
@@ -138,11 +138,13 @@ class TelelocustSmaModule(SMAObserver, Triggerable):
 
         if self.__deploy_enabled():
             log.info("Tearing down TeleLocust infrastructure...")
-            self.__kubectl("delete", DEPLOYMENT_YAML_PATH)
+            self.__kubectl("delete", DEPLOYMENT_YAML_PATH, namespace=self.config.namespace)
             log.info("TeleLocust infrastructure torn down.")
 
-    def __kubectl(self, command: str, file: str) -> str:
+    def __kubectl(self, command: str, file: str, namespace: str = None) -> str:
         cmdline = ["kubectl"] + command.split() + ["-f", file]
+        if namespace:
+            cmdline += ["-n", namespace]    
         log.debug(f"Executing kubectl command: {cmdline}")
         result = subprocess.run(
             cmdline,
@@ -182,7 +184,7 @@ class TelelocustSmaModule(SMAObserver, Triggerable):
         
         log.info(f'Started test run with token: {self.telelocust_client.token}')
         # todo: maybe we add a timeout here?
-        final_status = self.telelocust_client.wait_for_run_completion(polling_interval_seconds=1, max_retries=3)
+        final_status = self.telelocust_client.wait_for_run_completion(self.cancel, polling_interval_seconds=1, max_retries=3)
 
         if final_status is None:
             log.warning("Test run completion status is unknown due to polling or timeout errors.")
